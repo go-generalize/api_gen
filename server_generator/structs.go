fbe2f55f7a2f30b736b8101f0f8fd3d254f2be7d
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2020-06-30 20:41:49 +0900

    #26 remove unused break

--- server_generator/structs.go
+++ server_generator/structs.go
@@ -197,7 +197,6 @@ func createRequestParams(
 		switch types.ExprString(f.Type) {
 		case "string":
 			fDataType = StringRequestDataType
-			break
 		case "int", "int8", "int16", "int32", "int64",
 			"uint", "uint8", "uint16", "uint32", "uint64":
 			fDataType = IntegerRequestDataType
