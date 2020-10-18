126e8d7b8ba148dffce52044f01b44bc838e32f8
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2020-10-18 16:26:35 +0900

    feat template code

--- templates/frontend/api/classes/types.ts
+++ templates/frontend/api/classes/types.ts
@@ -1,6 +1,6 @@
 // THIS FILE IS A GENERATED CODE.
 // DO NOT EDIT THIS CODE BY YOUR OWN HANDS
-// generated version: unknown
+// generated version: 1.1.0
 
 export type PostCreateUserRequest = {
 	Gender: number;
