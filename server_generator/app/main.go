266c74a754d2701a6915a826ea6e66ffc0d6693f
54m <30588003+54m@users.noreply.github.com>
2021-03-03 12:47:54 +0900

    fix: follow the latest

--- server_generator/app/main.go
+++ server_generator/app/main.go
@@ -5,16 +5,16 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/go-generalize/api_gen/server_generator/sample/api_gen/bootstrap"
-	"github.com/go-generalize/api_gen/server_generator/sample/api_gen/props"
+	"github.com/go-generalize/api_gen/server_generator/sample/apigen"
+	"github.com/go-generalize/api_gen/server_generator/sample/apigen/props"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	bootstrap.Bootstrap(&props.ControllerProps{
+	apigen.Bootstrap(&props.ControllerProps{
 		TestKey: "foobar",
-	}, e, bootstrap.MiddlewareList{
+	}, e, apigen.MiddlewareList{
 		{
 			Path: "/service/user2/",
 			MiddlewareFunc: []echo.MiddlewareFunc{
