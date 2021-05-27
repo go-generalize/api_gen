e7934f64881425767e82774a00bb3be431a7d4cc
Tsuzuki Tsuchiya <8574909+cs3238-tsuzu@users.noreply.github.com>
2021-05-27 17:19:50 +0900

    Fix a bug that generated code is not formatted

--- pkg/server/controller_initializer.go
+++ pkg/server/controller_initializer.go
@@ -19,7 +19,7 @@ func (g *Generator) generateControllerInitializer(initializerPath, propsPackage 
 	defer fp.Close()
 
 	buf := bytes.NewBuffer(nil)
-	if err := g.controllerInitializerTemplate.Execute(fp, map[string]string{
+	if err := g.controllerInitializerTemplate.Execute(buf, map[string]string{
 		"ControllerPropsPackage": propsPackage,
 		"Package":                g.basePackage,
 		"AppVersion":             g.AppVersion,
