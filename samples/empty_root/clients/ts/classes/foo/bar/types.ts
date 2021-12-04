1792cceffac1f2a30026073da4ae07d848f58049
54m <30588003+54m@users.noreply.github.com>
2021-12-04 16:44:27 +0900

    update: generated code

--- samples/empty_root/clients/ts/classes/foo/bar/types.ts
+++ samples/empty_root/clients/ts/classes/foo/bar/types.ts
@@ -1,6 +1,6 @@
 // THIS FILE IS A GENERATED CODE.
 // DO NOT EDIT THIS CODE BY YOUR OWN HANDS
-// generated version: devel
+// generated version: (devel)
 
 export type PostUserRequest = {}
 export type PostUserResponse = {}
