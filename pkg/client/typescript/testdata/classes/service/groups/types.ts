f0728e66e24a1043157742b4c2bbb00d2b459233
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2021-12-16 16:04:56 +0900

    Fix test data for unit tests

---
+++ pkg/client/typescript/testdata/classes/service/groups/types.ts
@@ -0,0 +1,21 @@
+// THIS FILE IS A GENERATED CODE.
+// DO NOT EDIT THIS CODE BY YOUR OWN HANDS
+// generated version: devel
+
+export type Company = {
+	Metadata: Metadata;
+}
+export type Department = {
+	Metadata: Metadata;
+}
+export type GetGroupsRequest = {}
+export type GetGroupsResponse = {
+	Companies: Company[] | null;
+	Departments: Department[] | null;
+}
+export type Metadata = {
+	CreatedAt: string;
+	ID: string;
+	Name: string;
+	UpdatedAt: string;
+}
