9ec994d09ef7bba75fd71455d6009b0eec2b3b49
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2022-03-19 04:59:52 +0900

    Update POST :param/B E2E test in multipart for dart

--- e2e/multipart/tests/dart/run.dart
+++ e2e/multipart/tests/dart/run.dart
@@ -3,7 +3,7 @@ import '../../clients/dart/api_client.dart' as api_client;
 import '../../clients/dart/classes/types.dart' as types;
 import '../../clients/dart/classes/_param/types.dart' as ptypes;
 
-void main(List<String> args) async {
+void postA(String baseURL) async {
   final client = api_client.APIClient(args[0]);
 
   final req = types.PostARequest();
@@ -20,4 +20,29 @@ void main(List<String> args) async {
   if (result.message != "OK") {
     throw "Unexpected result: " + result.message;
   }
+}
+
+void postB(String baseURL) async {
+  final client = api_client.APIClient(args[0]);
+
+  final req = ptypes.PostBRequest();
+
+  req.file = MultipartFile.fromString('', '1');
+  req.files = [
+    MultipartFile.fromString('', '0', filename: '0.txt'),
+    MultipartFile.fromString('', '1', filename: '1.txt'),
+  ];
+  req.payload = 'payload';
+  req.param = 'param';
+
+  final result = await client.param.postB(req);
+
+  if (result.message != "OK") {
+    throw "Unexpected result: " + result.message;
+  }
+}
+
+void main(List<String> args) async {
+  await postA(args[0]);
+  await postB(args[0]);
 }
