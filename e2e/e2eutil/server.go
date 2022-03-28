51aed29614e9c3fa39ebca7713cf7881005a33fc
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2022-03-28 20:51:29 +0900

    Fix StartServer in e2eutil that it may return addr before the server is ready

--- e2e/e2eutil/server.go
+++ e2e/e2eutil/server.go
@@ -4,12 +4,34 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"net/http"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+func waitUntilServerReady(t *testing.T, addr string) {
+	t.Helper()
+
+	c := http.Client{
+		Timeout: 1 * time.Second,
+	}
+
+	for i := 0; i < 10; i++ {
+		_, err := c.Get(addr)
+
+		if err == nil {
+			return
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	t.Fatal("server is not ready")
+}
 
 // StartServer starts a server with given ctrlInitializer.
 func StartServer[Props any, Ctrl any](
@@ -35,7 +57,11 @@ func StartServer[Props any, Ctrl any](
 		e.Shutdown(context.Background())
 	}()
 
-	return fmt.Sprintf("http://127.0.0.1:%d", port), func() {
+	addr = fmt.Sprintf("http://127.0.0.1:%d", port)
+
+	waitUntilServerReady(t, addr)
+
+	return addr, func() {
 		close(ch)
 	}
 }
